7b2bf2c83ad11f543d7fa1b8eaab2a1b700b0929
Markus Neugebauer <mail@markusneugebauer.de>
2020-04-02 17:31:53 +0200

    implemented clear_all, renamed basket to cart, fixed show_order and show group_order when no orders where made, remove from cart fixed

--- src/bot.py
+++ src/bot.py
@@ -24,15 +24,16 @@ logger = logging.getLogger(__name__)
 url = 'https://www.izmir-kebap-friedrichshafen.de'
 menu = json.loads(get_menu(url))
 print(menu)
-basket = dict()
+cart = dict()
 all_user_data = dict()
 
-ONE, TWO, THREE, FOUR, FIVE = range(5)
+ONE, TWO, THREE, FOUR, FIVE, SIX = range(6)
 
-FIRST, SECOND, THIRD, FOURTH = range(4)
+FIRST, SECOND, THIRD, FOURTH, FIFTH = range(5)
 
 
 def show_categories(update, context):
+    print("test")
     query = update.callback_query
     bot = context.bot
 
@@ -76,7 +77,7 @@ def show_category(update, context):
     return THIRD
 
 
-def add_to_basket(update, context):
+def add_to_cart(update, context):
     query = update.callback_query
     bot = context.bot
     ids = json.loads(query.data)
@@ -86,21 +87,21 @@ def add_to_basket(update, context):
     chat_id = update.effective_chat.id
     user = update.effective_user
     print(user)
-    if chat_id in basket:
-        if user.id in basket[chat_id]:
-            if category_id in basket[chat_id][user.id]:
-                if product_id in basket[chat_id][user.id][category_id]:
-                    basket[chat_id][user.id][category_id][product_id] += 1
+    if chat_id in cart:
+        if user.id in cart[chat_id]:
+            if category_id in cart[chat_id][user.id]:
+                if product_id in cart[chat_id][user.id][category_id]:
+                    cart[chat_id][user.id][category_id][product_id] += 1
                 else:
-                    basket[chat_id][user.id][category_id][product_id] = 1
+                    cart[chat_id][user.id][category_id][product_id] = 1
             else:
-                basket[chat_id][user.id][category_id] = {product_id: 1}
+                cart[chat_id][user.id][category_id] = {product_id: 1}
         else:
-            basket[chat_id][user.id] = {category_id: {product_id: 1}}
+            cart[chat_id][user.id] = {category_id: {product_id: 1}}
     else:
-        basket[chat_id] = {user.id: {category_id: {product_id: 1}}}
+        cart[chat_id] = {user.id: {category_id: {product_id: 1}}}
 
-    print(basket)
+    print(cart)
     bot.edit_message_text(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
                           text='Added ' + menu[category_id]['products'][product_id][
@@ -109,39 +110,49 @@ def add_to_basket(update, context):
     return ConversationHandler.END
 
 
-def basket_inline_keyboard(update, context):
+def cart_inline_keyboard(update, context):
     bot = context.bot
     query = update.callback_query
 
     chat_id = update.effective_chat.id
     user = update.effective_user
 
-    user_basket = basket[chat_id][user.id]
-    keyboard = []
+    print(1)
+    print(cart)
+    if chat_id in cart.keys():
+        print(2)
+        if user.id in cart[chat_id].keys():
+            print(3)
+            user_cart = cart[chat_id][user.id]
+            keyboard = []
 
-    for category_id in user_basket.keys():
-        for product_id in user_basket[category_id].keys():
-            amount = user_basket[category_id][product_id]
-            name = menu[category_id]['products'][product_id]['name']
-            price = menu[category_id]['products'][product_id]['price']
+            for category_id in user_cart.keys():
+                for product_id in user_cart[category_id].keys():
+                    amount = user_cart[category_id][product_id]
+                    name = menu[category_id]['products'][product_id]['name']
+                    price = menu[category_id]['products'][product_id]['price']
 
-            keyboard.append([InlineKeyboardButton(str(amount) + "x " + name + " " + price,
+                    keyboard.append([InlineKeyboardButton(str(amount) + "x " + name + " " + price,
+                                                          callback_data=json.dumps({"category_id": category_id,
+                                                                                    "product_id": product_id}))])
+
+            keyboard.append([InlineKeyboardButton("Remove all products",
                                                   callback_data=json.dumps({"category_id": category_id,
-                                                                            "product_id": product_id}))])
+                                                                            "product_id": -1}))])
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            bot.edit_message_text(chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  text="Select product to remove from cart:",
+                                  reply_markup=reply_markup)
+            return FOURTH
 
-    keyboard.append([InlineKeyboardButton("Remove all products",
-                                          callback_data=json.dumps({"category_id": category_id,
-                                                                    "product_id": -1}))])
-    reply_markup = InlineKeyboardMarkup(keyboard)
     bot.edit_message_text(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
-                          text="Select product to remove from basket:",
-                          reply_markup=reply_markup)
-
-    return FOURTH
+                          text="No products in your cart!")
+    return ConversationHandler.END
 
 
-def remove_from_basket(update, context):
+def remove_from_cart(update, context):
     bot = context.bot
     query = update.callback_query
 
@@ -153,34 +164,48 @@ def remove_from_basket(update, context):
     product_id = ids['product_id']
 
     if product_id == -1:
-        del basket[chat_id][user_id]
+        print(cart)
+        del cart[chat_id][user_id]
+        print(cart)
+        if not bool(cart[chat_id]):
+            del cart[chat_id]
+        print(cart)
+        message = "All products removed from your cart!"
     else:
-        if basket[chat_id][user_id][category_id][product_id] > 1:
-            basket[chat_id][user_id][category_id][product_id] -= 1
+        if cart[chat_id][user_id][category_id][product_id] > 1:
+            cart[chat_id][user_id][category_id][product_id] -= 1
         else:
-            del basket[chat_id][user_id][category_id][product_id]
-            if not bool(basket[chat_id][user_id][category_id]):
-                del basket[chat_id][user_id][category_id]
+            del cart[chat_id][user_id][category_id][product_id]
+            if not bool(cart[chat_id][user_id][category_id]):
+                print(cart)
+                del cart[chat_id][user_id][category_id]
+            if not bool(cart[chat_id][user_id]):
+                print(cart)
+                del cart[chat_id][user_id]
+            if not bool(cart[chat_id]):
+                print(cart)
+                del cart[chat_id]
+        print(cart)
+        message = "Removed " + menu[category_id]['products'][product_id]['name'] + "from your cart. Your " \
+                                                                                   "cart: \n" + \
+                  str_user_cart(chat_id, user_id)['message']
 
-    print("los")
-    print(basket[chat_id][user_id])
+    print(cart)
     bot.edit_message_text(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
-                          text="Removed " + menu[category_id]['products'][product_id]['name'] +
-                               "from order. Your order: \n" +
-                               str_user_basket(basket[chat_id][user_id])['message'])
+                          text=message)
 
     return ConversationHandler.END
 
 
-def show_basket(update, context):
+def show_cart(update, context):
     bot = context.bot
     query = update.callback_query
 
     chat_id = update.effective_chat.id
     user = update.effective_user
 
-    message_and_price = str_user_basket(basket[chat_id][user.id])
+    message_and_price = str_user_cart(chat_id, user.id)
 
     bot.edit_message_text(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
@@ -189,43 +214,49 @@ def show_basket(update, context):
     return ConversationHandler.END
 
 
-def str_user_basket(basket_from_user):
-    price_basket = 0.0
+def str_user_cart(chat_id, user_id):
+    price_cart = 0.0
     message = ""
-    print(basket_from_user)
 
-    for category_id in basket_from_user.keys():
-        for product_id in basket_from_user[category_id].keys():
-            amount = basket_from_user[category_id][product_id]
-            name = menu[category_id]['products'][product_id]['name']
-            price = float(menu[category_id]['products'][product_id]['price'].replace(' €', ''))
+    if chat_id in cart.keys():
+        if user_id in cart[chat_id].keys():
+            cart_from_user = cart[chat_id][user_id]
+            for category_id in cart_from_user.keys():
+                for product_id in cart_from_user[category_id].keys():
+                    amount = cart_from_user[category_id][product_id]
+                    name = menu[category_id]['products'][product_id]['name']
+                    price = float(menu[category_id]['products'][product_id]['price'].replace(' €', ''))
 
-            price_basket += price * amount
-            print(price_basket)
-            message = message + str(amount) + "x " + name + " " + str(f'{price:.2f}') + " €" + "\n"
+                    price_cart += price * amount
+                    print(price_cart)
+                    message = message + str(amount) + "x " + name + " " + str(f'{price:.2f}') + " €" + "\n"
 
-    message = message + "Sum: " + str(f'{price_basket:.2f}') + " €\n"
+            message = message + "Sum: " + str(f'{price_cart:.2f}') + " €\n"
+            return {'message': message, 'price': price_cart}
+    message = "no items in cart\n"
+    return {'message': message, 'price': price_cart}
 
-    return {'message': message, 'price': price_basket}
 
-
-def show_group_basket(update, context):
+def show_group_cart(update, context):
     bot = context.bot
     query = update.callback_query
     chat_id = update.effective_chat.id
     message = ''
     chat_sum = 0.0
 
-    for user_id in basket[chat_id].keys():
-        user = all_user_data[user_id]
+    if chat_id in cart.keys():
+        for user_id in cart[chat_id].keys():
+            user = all_user_data[user_id]
 
-        message = message + str(user.username) + " aka " + str(user.first_name) + ":\n"
-        print(message)
-        message_and_price = str_user_basket(basket[chat_id][user_id])
-        chat_sum += message_and_price['price']
-        message = message + message_and_price['message']
+            message = message + str(user.username) + " aka " + str(user.first_name) + ":\n"
+            print(message)
+            message_and_price = str_user_cart(chat_id, user_id)
+            chat_sum += message_and_price['price']
+            message = message + message_and_price['message']
 
-    message = message + "Chat Sum: " + str(f'{chat_sum:.2f}') + " €"
+        message = message + "Chat Sum: " + str(f'{chat_sum:.2f}') + " €"
+    else:
+        message = "No orders made in this group chat!"
 
     bot.edit_message_text(chat_id=query.message.chat_id,
                           message_id=query.message.message_id,
@@ -238,32 +269,63 @@ def finish(update, context):
     return FIRST
 
 
+def clear_all_question(update, context):
+    bot = context.bot
+    query = update.callback_query
+    keyboard = [[InlineKeyboardButton("yes", callback_data="yes"), InlineKeyboardButton("no", callback_data="no")],
+                [InlineKeyboardButton("no, back to menu please", callback_data="menu")]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.edit_message_text(chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          text="Are you sure you want to delete all orders made in this chat?",
+                          reply_markup=reply_markup)
+    return FIFTH
+
+
 def clear_all(update, context):
     bot = context.bot
     query = update.callback_query
     chat_id = update.effective_chat.id
 
-    if chat_id in basket:
-        del basket[chat_id]
+    print(query.data)
 
-    bot.edit_message_text(chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          text="Deleted all orders for this chat!")
+    if query.data == "yes":
+        if chat_id in cart:
+            del cart[chat_id]
+        bot.edit_message_text(chat_id=query.message.chat_id,
+                              message_id=query.message.message_id,
+                              text="Deleted all orders for this chat!")
+        return ConversationHandler.END
+    if query.data == "no":
+        bot.edit_message_text(chat_id=query.message.chat_id,
+                              message_id=query.message.message_id,
+                              text="Nothing changed!")
+        return ConversationHandler.END
+    if query.data == "menu":
+        reply_markup = start_menu()
+        bot.edit_message_text(chat_id=query.message.chat_id,
+                              message_id=query.message.message_id,
+                              text="What do you want to do?",
+                              reply_markup=reply_markup)
+        return FIRST
 
-    return ConversationHandler.END
+
+def start_menu():
+    keyboard = [[InlineKeyboardButton("add food to cart", callback_data=str(ONE))],
+                [InlineKeyboardButton("remove food from cart", callback_data=str(TWO))],
+                [InlineKeyboardButton("your cart", callback_data=str(THREE))],
+                [InlineKeyboardButton("group cart", callback_data=str(FOUR))],
+                [InlineKeyboardButton("send order for group", callback_data=str(FIVE))],
+                [InlineKeyboardButton("clear all orders from chat", callback_data=str(SIX))]]
+
+    return InlineKeyboardMarkup(keyboard)
 
 
 def start(update, context):
     put_in_all_user_data(update.effective_user)
 
-    keyboard = [[InlineKeyboardButton("add to order", callback_data=str(ONE))],
-                [InlineKeyboardButton("remove from order", callback_data=str(TWO))],
-                [InlineKeyboardButton("your order", callback_data=str(THREE))],
-                [InlineKeyboardButton("group order", callback_data=str(FOUR))],
-                [InlineKeyboardButton("finish", callback_data=str(FIVE))]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
+    reply_markup = start_menu()
     update.message.reply_text("What do you want to do?", reply_markup=reply_markup)
 
     return FIRST
@@ -295,13 +357,15 @@ def main():
         entry_points=[CommandHandler('start', start)],
         states={
             FIRST: [CallbackQueryHandler(show_categories, pattern='^' + str(ONE) + '$'),
-                    CallbackQueryHandler(basket_inline_keyboard, pattern='^' + str(TWO) + '$'),
-                    CallbackQueryHandler(show_basket, pattern='^' + str(THREE) + '$'),
-                    CallbackQueryHandler(show_group_basket, pattern='^' + str(FOUR) + '$'),
-                    CallbackQueryHandler(finish, pattern='^' + str(FIVE) + '$')],
+                    CallbackQueryHandler(cart_inline_keyboard, pattern='^' + str(TWO) + '$'),
+                    CallbackQueryHandler(show_cart, pattern='^' + str(THREE) + '$'),
+                    CallbackQueryHandler(show_group_cart, pattern='^' + str(FOUR) + '$'),
+                    CallbackQueryHandler(finish, pattern='^' + str(FIVE) + '$'),
+                    CallbackQueryHandler(clear_all_question, pattern='^' + str(SIX) + '$')],
             SECOND: [CallbackQueryHandler(show_category)],
-            THIRD: [CallbackQueryHandler(add_to_basket)],
-            FOURTH: [CallbackQueryHandler(remove_from_basket)]
+            THIRD: [CallbackQueryHandler(add_to_cart)],
+            FOURTH: [CallbackQueryHandler(remove_from_cart)],
+            FIFTH: [CallbackQueryHandler(clear_all)]
         },
         fallbacks=[CommandHandler('start', start)]
     )
